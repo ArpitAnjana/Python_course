5fefe4ad7cc4e62155e108c858ab28c35005893c
Arpit Anjana <arpitanjana69@gmail.com>
2023-02-26 22:10:13 +0530

    tip calculator

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 # print("hello")
+# =======tip calculator:====
+food_amount = int(input("enter your payment: "))
+tip_amount = int(input("enter the tip percetage: "))
+
+total = int((food_amount*(tip_amount/100))+food_amount)
+
+print(f'Food Amount: ${food_amount}')
+print(f'Tip Amount: ${tip_amount}\n')
+print(f'total: 😀${total}')
