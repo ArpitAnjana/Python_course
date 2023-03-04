462831108e1c7639d491488d5a2184c92172285a
Arpit Anjana <arpitanjana69@gmail.com>
2023-03-04 21:30:16 +0530

    added file

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,1 @@
-():
-    fruit=['🍒','🍓','🍇']
-    for i in enumerate(fruit):
-        print(f'fruit: {i}')
-
-    for fruit in range(5):
-        print(fruit)
-        fruit.append('apple')
-
-fruits()
+man because i am batman'))
